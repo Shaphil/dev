9cf8df2f50440ae601666758437ac2c30b795e24
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2026-09-09 17:33:29 +0600

    Refactor utils
    * Return `CommandStatus` from `run_command`
    * Add function `sleep`

--- src/utils.rs
+++ src/utils.rs
@@ -1,20 +1,36 @@
+use colored::Colorize;
 use std::process::Command;
+use std::{thread, time};
 
-pub fn run_command(command: &[&str]) {
+pub enum CommandStatus {
+    SUCCEEDED,
+    FAILED,
+}
+
+pub fn run_command(command: &[&str]) -> CommandStatus {
     let mut cmd = Command::new(command[0]);
     cmd.args(&command[1..]);
     match cmd.status() {
         Ok(status) if status.success() => {
             if command[0] != "curl" && command[0] != "sudo" {
                 let output = cmd.output().expect("failed to execute command");
-                println!("{}", String::from_utf8_lossy(&output.stdout));
+                println!("{}", String::from_utf8_lossy(&output.stdout).green());
             }
+            CommandStatus::SUCCEEDED
         }
         Ok(status) => {
-            eprintln!("Command failed: {}", status);
+            let output = format!("Command failed: {}", status);
+            eprintln!("{}", output.red());
+            CommandStatus::FAILED
         }
         Err(e) => {
-            eprintln!("Error running command: {}", e);
+            let output = format!("Error running command: {}", e);
+            eprintln!("{}", output.red());
+            CommandStatus::FAILED
         }
     }
+}
+
+pub fn sleep(secs: u64) {
+    thread::sleep(time::Duration::from_secs(secs));
 }
