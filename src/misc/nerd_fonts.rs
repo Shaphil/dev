a717ad65e7e742cb4dc435fa6024146227b58bd5
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2026-09-17 17:34:09 +0600

    Refactor: Install Nerd Fonts with `Install Script` replacing installation via `yay`
    Source: https://github.com/ryanoasis/nerd-fonts#option-3-install-script

--- src/misc/nerd_fonts.rs
+++ src/misc/nerd_fonts.rs
@@ -1,19 +1,32 @@
 use crate::utils::cmd::DevTool;
+use crate::utils::logger;
 
 pub fn install_nerd_fonts() {
-    // install package manager `yay`
-    let mut tool: String = "yay".to_string();
-    let mut executor: String = "pacman".to_string();
-    let mut description: String = "package manager...".to_string();
-    let mut is_sudo = true;
-    let mut dev_tool = DevTool::new(tool, executor, description, is_sudo);
-    dev_tool.install();
+    let github_token = std::env::var("GH_TOKEN")
+        .unwrap_or_else(|_| "environment variable `GH_TOKEN` is not set or missing".to_string());
 
-    // install all nerd-fonts with `yay`
-    tool = "nerd-fonts-complete".to_string();
-    executor = "yay".to_string();
-    description = "fonts pack".to_string();
-    is_sudo = false;
-    dev_tool = DevTool::new(tool, executor, description, is_sudo);
-    dev_tool.install();
+    if github_token.is_empty() {
+        let message = format!(
+            "{}",
+            "GH_TOKEN not found. GitHub API rate \
+            limits may cause Nerd Fonts installation to fail."
+        );
+        logger::warning(&message);
+    }
+
+    let tool = "Nerd Fonts".to_string();
+    let executor = "pacman".to_string();
+    let description = "fonts pack".to_string();
+    let is_sudo = false;
+    let cmd = format!(
+        "export GH_TOKEN={} && \
+        curl -s https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/install.sh -o install.sh && \
+        chmod u+x install.sh && \
+        ./install.sh install all && \
+        rm install.sh",
+        github_token
+    );
+
+    let mut dev_tool = DevTool::new(tool, executor, description, is_sudo);
+    dev_tool.custom_install(&cmd);
 }
